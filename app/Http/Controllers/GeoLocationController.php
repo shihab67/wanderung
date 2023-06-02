5ba70ee81b7367cf0119c2d315215dee08342bb7
Sayedur Rahman Shihab <s.rahman3443@gmail.com>
2023-06-02 20:24:08 +0600

    installed alexpechkarev/google-maps package, implemented function to fetch data based on given coordinates

---
+++ app/Http/Controllers/GeoLocationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class GeoLocationController extends Controller
+{
+    public function places(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'latitude' => 'required',
+            'longitude' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+
+        $latlng = $request->latitude . ',' . $request->longitude;
+
+        $places = \GoogleMaps::load('nearbysearch')
+            ->setParam([
+                'location'  => $latlng,
+                'radius'    => isset($request->radius) ? $request->radius : '5000',
+            ])
+            ->get();
+
+        return response($places, 200);
+    }
+}
