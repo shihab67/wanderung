0827d9bdd0c27b9aea41ab43986ee206b436c483
Sayedur Rahman Shihab <s.rahman3443@gmail.com>
2023-06-02 20:22:46 +0600

    added auth:api guard in auth.php, changed route names

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->group(function () {
-    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+    Route::get('/places', 'GeoLocationController@places')->name('places.api');
 });
 
 Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
-    Route::post('/register', 'Auth\ApiAuthController@register')->name('register.api');
-    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+    Route::post('/signup', 'Auth\ApiAuthController@signup')->name('signup.api');
 });
