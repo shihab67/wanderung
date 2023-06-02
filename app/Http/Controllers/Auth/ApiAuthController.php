5d6300ca8d4e8f6e727f88afd661f11ca9136929
Sayedur Rahman Shihab <s.rahman3443@gmail.com>
2023-06-02 20:21:46 +0600

    Fixed Indentation

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -11,53 +11,56 @@ use Illuminate\Support\Str;
 
 class ApiAuthController extends Controller
 {
-    public function register(Request $request)
+    public function signup(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+        $validator      = Validator::make($request->all(), [
+            'email'     => 'required|string|email|max:255|unique:users',
+            'password'  => 'required|string|min:6|confirmed',
         ]);
+
         if ($validator->fails()) {
             return response(['errors' => $validator->errors()->all()], 422);
         }
-        $request['password'] = Hash::make($request['password']);
-        $request['remember_token'] = Str::random(10);
-        $user = User::create($request->toArray());
-        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-        $response = ['token' => $token];
-        return response($response, 200);
+
+        try {
+            $request['password']        = Hash::make($request['password']);
+            $request['remember_token']  = Str::random(10);
+            $user                       = User::create($request->toArray());
+            $token                      = $user->createToken('Laravel Password Grant Client')->accessToken;
+            $response                   = ['token' => $token];
+
+            return response($response, 200);
+        } catch (\Throwable $th) {
+            return response(['errors' => $th->getMessage()], 422);
+        }
     }
 
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255',
-            'password' => 'required|string|min:6',
+        $validator      = Validator::make($request->all(), [
+            'email'     => 'required|string|email|max:255',
+            'password'  => 'required|string|min:6',
         ]);
+
         if ($validator->fails()) {
             return response(['errors' => $validator->errors()->all()], 422);
         }
-        $user = User::where('email', $request->email)->first();
+
+        $user           = User::where('email', $request->email)->first();
+
         if ($user) {
             if (Hash::check($request->password, $user->password)) {
-                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-                $response = ['token' => $token];
+                $token      = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response   = ['token' => $token];
+
                 return response($response, 200);
             } else {
-                $response = ["message" => "Password mismatch"];
+                $response   = ["message" => "Password mismatch"];
                 return response($response, 422);
             }
         } else {
-            $response = ["message" => 'User does not exist'];
+            $response       = ["message" => 'User does not exist'];
             return response($response, 422);
         }
-    }
-
-    public function logout(Request $request)
-    {
-        $token = $request->user()->token();
-        $token->revoke();
-        $response = ['message' => 'You have been successfully logged out!'];
-        return response($response, 200);
     }
 }
